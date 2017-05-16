1617b4d4316e90998714ea86be51c7ac922364b8
zhangxinlin <zhangxinlin@meituan.com>
2017-05-16 19:38:01 +0800

    update

--- src/reactReduxAsync/lib/common.js
+++ src/reactReduxAsync/lib/common.js
@@ -7,6 +7,23 @@ export const createReducer = (initialState, handlers) => {
     };
 };
 
+export const createStore = (reducer) => {
+    let state = {};
+    const listeners = [];
+    const getState = () => state;
+    const dispatch = (action) => {
+        state = reducer(state, action);
+        listeners.forEach((listener) => listener());
+    };
+    const subscribe = (listener) => listeners.push(listener);
+
+    return {
+        getState,
+        dispatch,
+        subscribe,
+    };
+};
+
 export const sleep = (timeout) => {
     return new Promise((resolve) => {
         setTimeout(resolve, timeout);
