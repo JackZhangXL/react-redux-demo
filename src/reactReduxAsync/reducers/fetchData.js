0651051a2fad8e4100fa788d43724f52421a9f51
zhangxinlin <zhangxinlin@meituan.com>
2017-05-19 00:07:37 +0800

    final

--- src/reactReduxAsync/reducers/fetchData.js
+++ src/reactReduxAsync/reducers/fetchData.js
@@ -1,25 +1,23 @@
 import * as constant from '../configs/action';
+import { createReducer } from '../lib/common';
 
 const initialState = {
     fetching: false,
     data: null,
 };
 
-// 原先是number，现在state为object
-export default (state = initialState, action) => {
-    switch (action.type) {
-        case constant.REQUEST_DATA:
-            return {
-                ...state,
-                fetching: true,
-            };
-        case constant.RECEIVE_DATA:
-            return {
-                ...state,
-                fetching: false,
-                data: action.data,
-            };
-        default:
-            return state;
-    }
-};
+export default createReducer(initialState, {
+    [constant.REQUEST_DATA]: (state, action) => {
+        return {
+            ...state,
+            fetching: true,
+        };
+    },
+    [constant.RECEIVE_DATA]: (state, action) => {
+        return {
+            ...state,
+            fetching: false,
+            data: action.data,
+        };
+    },
+});
